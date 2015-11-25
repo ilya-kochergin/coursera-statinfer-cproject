f9cbaeaf4ff7712eeab18d1404429ba3d25cf3d6
ilya-kochergin <ikochergin@gmail.com>
2015-11-25 09:12:02 +0300

    after quiz submitting

--- hw3.R
+++ hw3.R
@@ -21,3 +21,21 @@ mn <- qt(.975,8)/3
 tresult <- t.test(mpg~cyl,data=subset(mtcars,cyl %in% c(4,6)),paired=FALSE,var.equal=TRUE)
 signif(tresult$conf.int,3)
 tresult
+# Suppose that 18 obese subjects were randomized, 9 each, to a new diet pill and
+# a placebo. Subjects' body mass indices (BMIs) were measured at a baseline and
+# again after having received the treatment or placebo for four weeks. The
+# average difference from follow-up to the baseline (followup - baseline) was 3
+# kg/m2 for the treated group and 1 kg/m2 for the placebo group. The
+# corresponding standard deviations of the differences was 1.5 kg/m2 for the
+# treatment group and 1.8 kg/m2 for the placebo group. The study aims to answer
+# whether the change in BMI over the four week period appear to differ between
+# the treated and placebo groups.
+
+# What is the pooled variance estimate? (to 2 decimal places)
+n1 <- n2 <- 9
+x1 <- -3  ##treated
+x2 <- 1  ##placebo
+s1 <- 1.5  ##treated
+s2 <- 1.8  ##placebo
+spsq <- ( (n1 - 1) * s1^2 + (n2 - 1) * s2^2) / (n1 + n2 - 2)
+S_p_2 <-  ((8))
--- quiz3_calc.R
+++ quiz3_calc.R
@@ -20,3 +20,29 @@ bme+c(-1,1)*qt(.975,8)*bsd/sqrt(9)
 # of the difference in weight have to be for the upper endpoint of the 95% T
 # confidence interval to touch 0? 2.10 1.50 2.60 0.30
 
+sigma <- 2*3/(qt(.975,8))
+sigma
+# Question 7
+
+#Suppose that 18 obese subjects were randomized, 9 each, to a new diet pill and
+#a placebo. Subjects’ body mass indices (BMIs) were measured at a baseline and
+#again after having received the treatment or placebo for four weeks. The
+#average difference from follow-up to the baseline (followup - baseline) was −3
+#kg/m2 for the treated group and 1 kg/m2 for the placebo group. The
+#corresponding standard deviations of the differences was 1.5 kg/m2 for the
+#treatment group and 1.8 kg/m2 for the placebo group. Does the change in BMI
+#over the four week period appear to differ between the treated and placebo
+#groups? Assuming normality of the underlying data and a common population
+#variance, calculate the relevant *90%* t confidence interval. Subtract in the
+#order of (Treated - Placebo) with the smaller (more negative) number first.
+
+xm <- -3 ; ym <- 1 
+xsd <- 1.5 ; ysd <- 1.8 
+Sp <- (1.5+1.8)/2
+Sp <- sqrt((1.5^2+1.8^2))/2
+xm-ym + c(-1,1)*qt(.95,18-2)*Sp*sqrt(2/9)
+# Question 6
+xm <- 4 ; xsd <- 0.6 
+ym <-  5 ; yvar <- 0.68
+Sp <- sqrt(mean(c(xvar,yvar)))
+xm-ym +c(-1,1)*qt(.975,10+10-2)*Sp*sqrt(2/10)
